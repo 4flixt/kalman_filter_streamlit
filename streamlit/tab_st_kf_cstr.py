b6f08995e4924952526b964e0b9b2101cf6502c0
Felix-Mac <felix.fiedler@tu-berlin.de>
2022-11-08 21:35:20 +0100

    Multiselect to CSTR example

--- streamlit/tab_st_kf_cstr.py
+++ streamlit/tab_st_kf_cstr.py
@@ -34,6 +34,14 @@ def main():
 
     col_left, col_center, col_right, col_dummy = st.columns((2, 3, 1, 2))
 
+    multiselect_options = col_right.multiselect(
+        'Show',
+        ['y', 'x', 'x_hat', 'x_hat_std'],
+        ['x']
+    )
+
+    st.write('You selected:', multiselect_options)
+
     n_options = 100
     options = np.logspace(-5, 0, n_options)
     col_right.markdown("Process noise $\sigma_q$")
@@ -100,60 +108,65 @@ def main():
         p_3sig = (KF.x_data_observer[:, i, 0] + 3 * sig_x_i).flatten()
         m_3sig = (KF.x_data_observer[:, i, 0] - 3 * sig_x_i).flatten()
 
-        fig.add_trace(
-            go.Scatter(
-                x=KF.time, y=m_3sig, name='c_{}'.format('abc'[i]),
-                line = dict(
-                    width=0, 
-                    #color=DEFAULT_PLOTLY_COLORS[1]
+        if 'x_hat_std' in multiselect_options:
+            fig.add_trace(
+                go.Scatter(
+                    x=KF.time, y=m_3sig, name='c_{}'.format('abc'[i]),
+                    line = dict(
+                        width=0, 
+                        #color=DEFAULT_PLOTLY_COLORS[1]
+                        ),
+                    showlegend=False,
                     ),
-                showlegend=False,
-                ),
-            row=i+1, col=1)
-        fig.add_trace(
-            go.Scatter(
-                x=KF.time, y=p_3sig, name='c_{}'.format('abc'[i]), fill='tonexty', 
-                legendgroup='pm3sigma', legendgrouptitle_text='+/- 3 sigma'.format('abc'[i]),
-                line = dict(
-                    width=0, 
-                    #color=DEFAULT_PLOTLY_COLORS[1]
+                row=i+1, col=1)
+    
+            fig.add_trace(
+                go.Scatter(
+                    x=KF.time, y=p_3sig, name='c_{}'.format('abc'[i]), fill='tonexty', 
+                    legendgroup='pm3sigma', legendgrouptitle_text='+/- 3 sigma'.format('abc'[i]),
+                    line = dict(
+                        width=0, 
+                        #color=DEFAULT_PLOTLY_COLORS[1]
+                        ),
                     ),
-                ),
-            row=i+1, col=1)
+                row=i+1, col=1)
+        if 'x' in multiselect_options:
+            fig.add_trace(
+                go.Scatter(
+                    x=KF.time, y=KF.x_data[:, i, 0], 
+                    line = dict(
+                        dash='dot',
+                        width=2,
+                        ),
+                    name='c_{}'.format('abc'[i]),
+                    legendgroup='true', legendgrouptitle_text='true states'
+                    ),
+                row=i+1, col=1)
+        if 'x_hat' in multiselect_options:
+            fig.add_trace(
+                go.Scatter(
+                    x=KF.time, y=KF.x_data_observer[:, i, 0],
+                    line = dict(
+                        width=2,
+                        ),
+                    #line_color=DEFAULT_PLOTLY_COLORS[1],
+                    name='c_{}'.format('abc'[i]),
+                    legendgroup='est', legendgrouptitle_text='est states'
+                    ),
+                row=i+1, col=1)
+        
 
+    if 'y' in multiselect_options:
         fig.add_trace(
             go.Scatter(
-                x=KF.time, y=KF.x_data[:, i, 0], 
-                line = dict(
-                    dash='dot',
-                    width=2,
-                    ),
-                name='c_{}'.format('abc'[i]),
-                legendgroup='true', legendgrouptitle_text='true states'
+                x=KF.time, y=KF.y_data[:, 0, 0], name='measured',
+                marker=dict(
+                    size=4, 
+                    #color=DEFAULT_PLOTLY_COLORS[4], 
+                    opacity=0.8),
+                mode='markers',
                 ),
-            row=i+1, col=1)
-        fig.add_trace(
-            go.Scatter(
-                x=KF.time, y=KF.x_data_observer[:, i, 0],
-                line = dict(
-                    width=2,
-                    ),
-                #line_color=DEFAULT_PLOTLY_COLORS[1],
-                name='c_{}'.format('abc'[i]),
-                legendgroup='est', legendgrouptitle_text='est states'
-                ),
-            row=i+1, col=1)
-
-    fig.add_trace(
-        go.Scatter(
-            x=KF.time, y=KF.y_data[:, 0, 0], name='measured',
-            marker=dict(
-                size=4, 
-                #color=DEFAULT_PLOTLY_COLORS[4], 
-                opacity=0.8),
-            mode='markers',
-            ),
-        row=2, col=1)
+            row=2, col=1)
 
     fig.update_xaxes(title_text="time [s]", row=3, col=1)
     fig.update_yaxes(title_text="c_a [kmol/l]", row=1, col=1)
@@ -161,7 +174,7 @@ def main():
     fig.update_yaxes(title_text="c_c [kmol/l]", row=3, col=1)
 
     fig.update_layout(height=800, template='plotly_dark',
-        margin={"r":0,"t":0,"l":0,"b":0}
+        margin={"r":0,"t":30,"l":0,"b":0}
     )
     col_center.plotly_chart(fig, use_container_width=True)
 
